60da52c5edc3ad3ad91b72efde1c3a787f500220
dr304113 <dr304113@ohio.edu>
2022-09-16 19:09:17 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A to-do list, RESTful web application utilizing MVC design pattern and Spring Boot. 
+
+Utilizes CRUD functionalities to manage data persistence to/from MySQL database 
---
+++ src/main/java/com/sg/todoapi/App.java
@@ -0,0 +1,16 @@
+package com.sg.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author dr304
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
